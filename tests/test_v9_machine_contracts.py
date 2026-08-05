de464d26f5a7d3aeabe0cb5d9d63dd3dab998fc3
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-06 01:38:33 +0900

    test: wire Base C0 Pilot into v9 contracts

--- tests/test_v9_machine_contracts.py
+++ tests/test_v9_machine_contracts.py
@@ -13,6 +13,12 @@ from tests.test_godot_editor_transaction_adapter import (
 from tests.test_godot_editor_transaction_adapter_runtime import (
     GodotEditorTransactionAdapterRuntimeTests as _GodotEditorTransactionAdapterRuntimeTests,
 )
+from tests.test_godot_multi_project_pilot import (
+    GodotMultiProjectPilotTests as _GodotMultiProjectPilotTests,
+)
+from tests.test_godot_multi_project_pilot_adversarial import (
+    GodotMultiProjectPilotAdversarialTests as _GodotMultiProjectPilotAdversarialTests,
+)
 from tests.test_godot_live_editor_adapter_resolution import (
     GodotAdapterResolutionTests as _GodotAdapterResolutionTests,
 )
