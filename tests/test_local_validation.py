1398f08aae7856a295c0fe66e1a0119b4b726655
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-08 01:39:25 +0900

    test: include project asset vault in required local regression

--- tests/test_local_validation.py
+++ tests/test_local_validation.py
@@ -49,6 +49,9 @@ from tests.test_game_entitlement_integrity_drm_capability import (
 from tests.test_local_ci_fallback import (
     LocalCiFallbackTests as _LocalCiFallbackTests,
 )
+from tests.test_project_asset_vault import (
+    ProjectAssetVaultTests as _ProjectAssetVaultTests,
+)
 from tools import run_local_validation as runner
 
 
