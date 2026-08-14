bbe4bd6021b50930f50446d2bd263e2670d66061
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-15 01:06:31 +0900

    fix: keep A2 package import dependency-light

--- tools/loop_a2_runtime/__init__.py
+++ tools/loop_a2_runtime/__init__.py
@@ -1,31 +1,13 @@
 """Bounded A2 runtime primitives for approved Loop Engineering packages."""
 
-from .authority_snapshot import (
-    AuthorityFile,
-    AuthoritySnapshot,
-    AuthoritySnapshotError,
-    capture_authority_snapshot,
-)
-from .candidate_verification import (
-    CandidateVerificationError,
-    ProjectTestCandidateVerifier,
-    VerificationEvidenceMailbox,
-)
 from .protocol import ProtocolError, ReviewResult, RunRequest, WorkerResult
 from .runner import A2Runtime, RunOutcome
 
 __all__ = [
     "A2Runtime",
-    "AuthorityFile",
-    "AuthoritySnapshot",
-    "AuthoritySnapshotError",
-    "CandidateVerificationError",
-    "ProjectTestCandidateVerifier",
     "ProtocolError",
     "ReviewResult",
     "RunOutcome",
     "RunRequest",
-    "VerificationEvidenceMailbox",
     "WorkerResult",
-    "capture_authority_snapshot",
 ]
