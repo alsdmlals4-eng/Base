1a0129ec1e0ede8eb14b6a21670c0e5c920d97bf
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-09-08 02:15:15 +0900

    test: align Windows cost contract with identical archive pin

--- tests/test_ci_workflow_cost_policy.py
+++ tests/test_ci_workflow_cost_policy.py
@@ -126,7 +126,7 @@ class CiWorkflowCostPolicyTests(unittest.TestCase):
         self.assertNotIn("- name: Install Windows publication dependencies", body)
         self.assertNotIn("choco install libreoffice-fresh", body)
         for term in (
-            '$libreOfficeVersion = "26.2.3"',
+            '$libreOfficeVersion = "26.2.3.2"',
             '$libreOfficeSha256 = "468d1fb3880af3bcddac002e9054155912c70b45d105bfa1c82036f33456133d"',
             "https://download.documentfoundation.org/libreoffice/stable/$libreOfficeVersion/win/x86_64/LibreOffice_${libreOfficeVersion}_Win_x86-64.msi",
             "Get-FileHash -Algorithm SHA256",
